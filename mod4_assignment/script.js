b6f3771de5bfd5cdd5e4d7eb9eed6ee922321026
Georg Volov <georgvolov@gmail.com>
2025-04-13 18:56:12 +0300

    module 4 assignment

---
+++ mod4_assignment/script.js
@@ -0,0 +1,20 @@
+(function () {
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  
+    for (let i = 0; i < names.length; i++) {
+      let name = names[i];
+  
+      if (name.charAt(0).toLowerCase() === "j") {
+        byeSpeaker.speak(name);
+      } else {
+        helloSpeaker.speak(name);
+      }
+    }
+})();
+
+let i = function() {
+    let pede = "relina";
+    console.log("hello" + " " + pede);
+};
+
+i();
